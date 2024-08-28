c020ade829388747344751c3a207d7517452f2eb
hanqingxin.vite <hanqingxin.vite@bytedance.com>
2024-08-28 20:17:40 +0800

    feat: some damage

--- src/modules/being.ts
+++ src/modules/being.ts
@@ -1,7 +1,7 @@
+import { EffectiveDamage } from './damage';
 import { Timeline } from '../interaction/timeline';
 
 export interface IBeing {
-  max_life: number;
   life: number;
   energy_shield: number;
 
@@ -41,6 +41,11 @@ export interface IBeing {
 
 export class Being {
   life: number;
+  max_life: number;
+  add_life: number;
+  init_life: number;
+  life_increase_rate: number;
+
   energy_shield: number;
 
   strength: number;
@@ -52,6 +57,8 @@ export class Being {
   fire_damage: [number, number, number];
   frost_damage: [number, number, number];
   poison_damage: [number, number, number];
+
+  damage_increase_rate: number;
 
   level: number;
   exp: number;
@@ -78,29 +85,48 @@ export class Being {
   timeline: Timeline = new Timeline([]);
 
   constructor(being: IBeing) {
-    this.life = being.life;
+    this.life = 0;
+    this.init_life = being.life;
+    this.max_life = 0;
+    this.add_life = 0;
+    this.life_increase_rate = 0;
+
     this.energy_shield = being.energy_shield;
+
     this.strength = being.strength;
     this.agileness = being.agileness;
+    this.intelligence = being.intelligence;
+
     this.physical_damage = being.physical_damage;
     this.electric_damage = being.electric_damage;
     this.fire_damage = being.fire_damage;
     this.frost_damage = being.frost_damage;
     this.poison_damage = being.poison_damage;
-    this.intelligence = being.intelligence;
+    this.damage_increase_rate = 0;
+
     this.level = being.level;
     this.exp = being.exp;
+
     this.name = being.name;
+
     this.base_crit_rate = being.base_crit_rate;
     this.crit_rate = being.crit_rate;
     this.crit_dmg = being.crit_dmg;
+
     this.attack_speed = being.attack_speed;
     this.action_duration = being.action_duration;
+
     this.block_rate = being.block_rate;
     this.block_dmg = being.block_dmg;
+
     this.dodge_value = being.dodge_value;
     this.hit_value = being.hit_value;
+
     this.act_end = 0;
+  }
+
+  get isFullLife() {
+    return this.life === this.max_life;
   }
 
   initActTime(now: number) {
@@ -108,8 +134,20 @@ export class Being {
     this.act_end = now + action_duration / (1 + attack_speed / 100);
   }
 
+  computeLife() {
+    const { life_increase_rate, add_life, init_life, level, strength } = this;
+    const isFullLife = this.isFullLife;
+    this.max_life = Math.floor(
+      (init_life + add_life) * life_increase_rate + strength / 2 + level * 15
+    );
+    if (isFullLife) {
+      this.life = this.max_life;
+    }
+  }
+
   init(now: number) {
     this.initActTime(now);
+    this.computeLife();
   }
 
   // 受到伤害
@@ -142,8 +180,15 @@ export class Being {
     return this.enemy;
   }
 
-  attack() {
-    const {} = this;
+  attack(): EffectiveDamage {
+    const {
+      physical_damage,
+      fire_damage,
+      frost_damage,
+      poison_damage,
+      electric_damage,
+      damage_increase_rate,
+    } = this;
   }
 
   act() {
